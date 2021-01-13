c3059fbfcf35bb7e24fdb6ce3511a5dc508fd1d5
Star-Gibson <sgibsoncoding@gmail.com>
2021-01-13 17:16:58 -0500

    Base Code for Header

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Implement ReactRouter Routes  */}
+      {/* Keeping Components currently for test purposes */}
+      <Header />
+    
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-3">Star Gibson - Web Developer</h1>
+                <p class="lead display-4">Raleigh, NC</p>
+            </div>
+        </div>
+    )
+}
+
+export default Header
